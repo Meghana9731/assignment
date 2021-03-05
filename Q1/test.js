669bde1270e46b3b3a1752e3117936be52cacc52
Meghana9731 <78469882+Meghana9731@users.noreply.github.com>
2021-03-05 16:23:51 +0530

    Initial commit

---
+++ Q1/test.js
@@ -0,0 +1,15 @@
+
+function myFunction() {
+	var arr = [];
+	var mydata1 = JSON.parse(data);
+	var mydata2 = JSON.parse(datas);
+	var mydata3 = JSON.parse(datat);
+	
+	arr = Array.prototype.concat(mydata1, mydata2, mydata3)
+	
+
+	console.log(arr);
+		console.log(mydata1);
+			console.log(mydata2);
+				console.log(mydata3);
+}
